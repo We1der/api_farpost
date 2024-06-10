33fe0b78301ed35d8101145b45a52508e94788ce
Mikhail <offrolov.ms@gmail.com>
2024-06-10 23:33:48 +0300

    create models and mirgrations

--- api_farpost/api_farpost/settings.py
+++ api_farpost/api_farpost/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'announcements.apps.AnnouncementsConfig',
+    'users.apps.UsersConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
